4ae01b52cedbf39ca23e57d8b59f768b8c8e25df
Boyce <joshuadboyce@gmail.com>
2016-11-01 10:26:31 -0500

    Edited Platforms and Dimensions

--- game.js
+++ game.js
@@ -1,42 +1,105 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
 (function () {
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     window.requestAnimationFrame = requestAnimationFrame;
 })();
 
+
+
+
+
+// CANVAS
+
+//***************************************************************************
+
+
 var canvas = document.getElementById("canvas"),
+
     ctx = canvas.getContext("2d"),
-    width = 500,
-    height = 200,
+    width = 1000,
+    height = 500,
     player = {
-        x: width / 2,
-        y: height - 15,
-        width: 5,
-        height: 5,
-        speed: 3,
-        velX: 0,
-        velY: 0,
-        jumping: false,
-        grounded: false
-    },
+                x: width / 2,
+                y: height - 15,
+                width: 5,
+                height: 5,
+                speed: 3,
+                velX: 0,
+                velY: 0,
+                jumping: false,
+                grounded: false
+            },
+
     keys = [],
     friction = 0.8,
     gravity = 0.3;
 
 var boxes = [];
 
-// dimensions
+
+//***************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+// DIMENSIONS AND PLATFORMS (X = H, Y = V)
+
+//***************************************************************************
+
+
+
+//**************************
+// LEFT WALL
+
 boxes.push({
     x: 0,
-    y: 0,
+    y: 100,
     width: 10,
     height: height
 });
+
+//**************************
+
+
+
+
+//**************************
+// GROUND
+
 boxes.push({
     x: 0,
     y: height - 2,
     width: width,
     height: 50
 });
+
+//**************************
+
+
+
+
+//**************************
+// RIGHT WALL
+
 boxes.push({
     x: width - 10,
     y: 0,
@@ -44,30 +107,50 @@ boxes.push({
     height: height
 });
 
+//**************************
+
+
+
+
+//**************************
+//PLATFORMS
+
 boxes.push({
-    x: 120,
-    y: 10,
-    width: 80,
-    height: 80
+    x: 30, //h
+    y: 450, //v
+    width: 300,
+    height: 10
 });
+
 boxes.push({
     x: 170,
     y: 50,
     width: 80,
     height: 80
 });
+
 boxes.push({
     x: 220,
     y: 100,
     width: 80,
     height: 80
 });
+
 boxes.push({
     x: 270,
     y: 150,
     width: 40,
     height: 40
 });
+
+
+//***************************************************************************
+
+
+
+
+
+
 
 canvas.width = width;
 canvas.height = height;
@@ -127,24 +210,51 @@ function update() {
     player.x += player.velX;
     player.y += player.velY;
 
+
+
+//*************************************************************************
+
+// Player Attributes
+
     ctx.fill();
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "blue";
     ctx.fillRect(player.x, player.y, player.width, player.height);
+
+//*************************************************************************
+
+
+
+
+
+
 
     requestAnimationFrame(update);
 }
 
 function colCheck(shapeA, shapeB) {
+
+
+
     // get the vectors to check against
     var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
         vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
+
+
+
+
         // add the half widths and half heights of the objects
         hWidths = (shapeA.width / 2) + (shapeB.width / 2),
         hHeights = (shapeA.height / 2) + (shapeB.height / 2),
         colDir = null;
 
+
+
+
     // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
-    if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+
+    if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) 
+
+    {
         // figures out on which side we are colliding (top, bottom, left, or right)
         var oX = hWidths - Math.abs(vX),
             oY = hHeights - Math.abs(vY);
