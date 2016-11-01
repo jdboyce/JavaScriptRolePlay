c3de4f490a5189a4f8162edf55e87f8c9a2b91e3
JakeDrohman <jkdrohman@gmail.com>
2016-11-01 17:16:38 -0500

    added prototypes

---
+++ textGame.js
@@ -0,0 +1,20 @@
+"use strict";
+function character(startingHealth){
+	this.health=startingHealth;
+}
+function player(){
+	this.armor=20;
+}
+function zombie(){
+	this.meleeDamage=10;
+}
+
+
+
+
+
+
+function initGame(){
+	var user= new player.assign
+}
+initGame()
