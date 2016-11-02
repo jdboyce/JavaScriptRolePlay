1a9d60782ef20d4e259f514f673c8ef4f4fcdced
JakeDrohman <jkdrohman@gmail.com>
2016-11-02 16:19:34 -0500

    added call apply and bind

--- textGame.js
+++ textGame.js
@@ -3,7 +3,7 @@
 // 	this.health=startingHealth;
 // }
 function player(){
-	this.startingHealth=100;
+	this.health=100;
 	this.armor=20;
 	this.gold;
 }
@@ -19,28 +19,28 @@ function calculateBattleResult(playerInput){
 	//returns a true if the player wins returns false when the enemy wins
 	let computerInput=(Math.round(Math.random()*3));
 	let outputResult=(3+playerInput-computerInput)%3;
+	let zombieDamage=(Math.round(Math.random()*10));
 	if(outputResult===1){
-		return playerWins()
+		return function(){
+			this.gold+=15;
+		}
 	}
 	else if (outputResult===2) {
-		return playerLoses()
+		return function(){
+
+			if(this.health<=zombieDamage){
+				return gameOver();
+			}
+			else{
+				this.health-=zombieDamage;
+			}
+		}
 	}
 	else{
 		return calculateBattleResult(playerInput)
 	}
 }
-function playerWins(){
-	player.gold+=15;
-}
-function playerLoses(){
-	let healthToLose=(Math.round(Math.random()*10));
-	if(player.health<=healthToLose){
-		return gameOver();
-	}
-	else{
-		player.health-=healthToLose;
-	}
-}
+
 function gameOver(){
 
 }
@@ -48,13 +48,19 @@ function gameOver(){
 
 
 function startAlienStory(){
-
+	//somewhere in the story
+	let playerInput;
+	calculateBattleResult.call(/*whatever the user's name is*/,playerInput)
 }
 function startCowboyStory(){
-
+	let playerInput;
+	let playerInputArrayThatIsLiterallyOneObject=[playerInput]
+	calculateBattleResult.apply(/*whatever the user's name is*/,playerInputArrayThatIsLiterallyOneObject)
 }
 function startNinjaStory(){
-
+	let playerInput;
+	var battleWithNinja=calculateBattleResult.bind(/*this person's name*/);
+	battleWithNinja(playerInput)
 }
 function initGame(){
 	var user= new player.assign;
